11c1e289501fe640bf18cbd5976a7386dc50cc62
Krompy <kompennet@gmail.com>
2017-05-26 21:08:00 +0300

    XML-serialization

--- XMLSerializer/XMLSerializer/XMLSerializer.cs
+++ XMLSerializer/XMLSerializer/XMLSerializer.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using Lab_4.Books;
+﻿using Lab_4.Books;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
 
 namespace Lab_4.Helpers.Serialization
 {
     class XMLSerializer : ISerializer
     {
-        public T Deserialize<T>(string smth)
-        {
-            throw new NotImplementedException();
-            //TODO
-        }
-
         public string Serialize(Book smth)
         {
-            throw new NotImplementedException();
             //TODO
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Book));
+            StringWriter writer = new StringWriter();
+            serializer.Serialize(writer, smth);
+            return writer.ToString();
+        }
+
+        public T Deserialize<T>(string smth)
+        {
+            //TODO
+            throw new NotImplementedException();
         }
     }
 }
